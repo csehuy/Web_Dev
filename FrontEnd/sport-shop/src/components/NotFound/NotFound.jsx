b9a4dc260de74bf0822e8e77c664f2fbb5b30d54
huy <trankhachuy.cse@gmail.com>
2024-04-28 13:29:41 +0700

    Setup repository

---
+++ FrontEnd/sport-shop/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./NotFound.module.scss";
+
+const NotFound = () => (
+  <img
+    src="https://www.pngitem.com/pimgs/m/561-5616833_image-not-found-png-not-found-404-png.png"
+    alt="Not Found"
+  />
+);
+
+export default NotFound;
---
+++ FrontEnd/sport-shop/src/features/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+CartPage.propTypes = {};
+
+function CartPage(props) {
+  return <div>Cart Page</div>;
+}
+
+export default CartPage;
